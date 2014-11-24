5b7406f97863f735365ad09e4173a264dcfa89ce
Eric Buehl <eric.buehl@gmail.com>
2014-11-23 17:16:16 -0800

    give pods another chance

---
+++ shadyglade/shadyglade-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  shadyglade-Bridging-Header.h
+//  shadyglade
+//
+//  Created by Eric Buehl on 11/23/14.
+//  Copyright (c) 2014 Eric Buehl. All rights reserved.
+//
+
+#ifndef shadyglade_shadyglade_Bridging_Header_h
+#define shadyglade_shadyglade_Bridging_Header_h
+
+#import "AFNetworking.h"
+
+#endif
