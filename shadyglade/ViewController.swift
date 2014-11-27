a17aaf251abcd1a11fca342180b882a0d4119dee
Eric Buehl <eric.buehl@gmail.com>
2014-11-27 14:50:23 -0800

    completely swap out swiftyjson and alamofire for afnetworking

--- shadyglade/ViewController.swift
+++ shadyglade/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
-import Alamofire
 
 let shadeBase = "http://192.168.1.54:7378"
 let poolBase = "http://192.168.1.50:7379"
 //let base = "https://192.168.1.51:7379" // rpi01
+
+let manager = AFHTTPRequestOperationManager()
+
 
 
 class ViewController: UIViewController {
@@ -19,6 +21,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         println("viewDidLoad")
+        manager.requestSerializer! = AFJSONRequestSerializer()
         var timer = NSTimer.scheduledTimerWithTimeInterval(10, target: self, selector: Selector("reloadView"), userInfo: nil, repeats: true)
 
     }
@@ -31,27 +34,11 @@ class ViewController: UIViewController {
         println("reloading view...")
         // Do any additional setup after loading the view, typically from a nib.
         for i in 1...4 {
-            Alamofire.request(.GET, shadeBase + "/resources/sensors/shade\(i)/state")
-                .responseJSON { (request, response, data, error) in
-                    println(error)
-                    if let e = error? {
-                        var alert = UIAlertController(title: "Error", message: "Couldn't get shade status", preferredStyle: UIAlertControllerStyle.Alert)
-                        alert.addAction(UIAlertAction(title: "Boooo", style: .Default, handler: { action in
-                            switch action.style{
-                            case .Default:
-                                println("default")
-                                
-                            case .Cancel:
-                                println("cancel")
-                                
-                            case .Destructive:
-                                println("destructive")
-                            }
-                        }))
-                        self.presentViewController(alert, animated: true, completion: nil)
-                        return
-                    }
-                    var updown = ((data as Int) & 1) == 1 ? true : false  // 0 = up, 1 = down, 2 = moving up, 3 = moving down
+            manager.GET(shadeBase + "/resources/sensors/shade\(i)/state", //"http://ip.jsontest.com/",
+                parameters: nil,
+                success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+                    let responseDict = responseObject as NSDictionary
+                    let updown = ((responseDict["state"]! as Int) & 1) == 1 ? true : false  // 0 = up, 1 = down, 2 = moving up, 3 = moving down
                     switch i as Int {
                     case 1:
                         self.shade1switch.setOn(updown, animated: false)
@@ -64,13 +51,10 @@ class ViewController: UIViewController {
                     default:
                         println("huh?")
                     }
-            }
-        }
-        Alamofire.request(.GET, poolBase + "/resources/sensors/spa/state")
-            .responseJSON { (request, response, data, error) in
-                println(error)
-                if let e = error? {
-                    var alert = UIAlertController(title: "Error", message: "Couldn't get pool status", preferredStyle: UIAlertControllerStyle.Alert)
+                    
+                },
+                failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+                    var alert = UIAlertController(title: "Error", message: "Couldn't get shade status", preferredStyle: UIAlertControllerStyle.Alert)
                     alert.addAction(UIAlertAction(title: "Boooo", style: .Default, handler: { action in
                         switch action.style{
                         case .Default:
@@ -85,43 +69,66 @@ class ViewController: UIViewController {
                     }))
                     self.presentViewController(alert, animated: true, completion: nil)
                     return
-                }
+            })
 
-                switch data as Int {
-                case 0:
-                    self.spaState.textColor = UIColor.darkGrayColor()
-                    self.spaState.text = "Off"
-                    self.spaSwitch.on = false
-                    self.spaSwitch.enabled = true
-                case 1:
-                    self.spaState.textColor = UIColor.greenColor()
-                    self.spaState.text = "On"
-                    self.spaSwitch.on = true
-                    self.spaSwitch.enabled = true
-                case 2:
-                    self.spaState.textColor = UIColor.grayColor()
-                    self.spaState.text = "Starting..."
-                    self.spaSwitch.on = true
-                    self.spaSwitch.enabled = false
-                case 3:
-                    self.spaState.textColor = UIColor.yellowColor()
-                    self.spaState.text = "Warming..."
-                    self.spaSwitch.on = true
-                    self.spaSwitch.enabled = true
-                case 4:
-                    self.spaState.textColor = UIColor.orangeColor()
-                    self.spaState.text = "Standby"
-                    self.spaSwitch.on = true
-                    self.spaSwitch.enabled = true
-                case 5:
-                    self.spaState.textColor = UIColor.grayColor()
-                    self.spaState.text = "Stopping..."
-                    self.spaSwitch.on = false
-                    self.spaSwitch.enabled = false
-                default:
-                    println("huh?")
-                }
         }
+        manager.GET(poolBase + "/resources/sensors/spa/state", parameters:nil, success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+            let responseDict = responseObject as NSDictionary
+
+            switch responseDict["state"]! as Int {
+            case 0:
+                self.spaState.textColor = UIColor.darkGrayColor()
+                self.spaState.text = "Off"
+                self.spaSwitch.on = false
+                self.spaSwitch.enabled = true
+            case 1:
+                self.spaState.textColor = UIColor.greenColor()
+                self.spaState.text = "On"
+                self.spaSwitch.on = true
+                self.spaSwitch.enabled = true
+            case 2:
+                self.spaState.textColor = UIColor.grayColor()
+                self.spaState.text = "Starting..."
+                self.spaSwitch.on = true
+                self.spaSwitch.enabled = false
+            case 3:
+                self.spaState.textColor = UIColor.yellowColor()
+                self.spaState.text = "Warming..."
+                self.spaSwitch.on = true
+                self.spaSwitch.enabled = true
+            case 4:
+                self.spaState.textColor = UIColor.orangeColor()
+                self.spaState.text = "Standby"
+                self.spaSwitch.on = true
+                self.spaSwitch.enabled = true
+            case 5:
+                self.spaState.textColor = UIColor.grayColor()
+                self.spaState.text = "Stopping..."
+                self.spaSwitch.on = false
+                self.spaSwitch.enabled = false
+            default:
+                println("huh?")
+            }
+            
+            }, failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+                println(error)
+                var alert = UIAlertController(title: "Error", message: "Couldn't get pool status", preferredStyle: UIAlertControllerStyle.Alert)
+                alert.addAction(UIAlertAction(title: "Boooo", style: .Default, handler: { action in
+                    switch action.style{
+                    case .Default:
+                        println("default")
+                        
+                    case .Cancel:
+                        println("cancel")
+                        
+                    case .Destructive:
+                        println("destructive")
+                    }
+                }))
+                self.presentViewController(alert, animated: true, completion: nil)
+                return
+        })
+
 
     }
 
@@ -142,13 +149,12 @@ class ViewController: UIViewController {
 
     
     @IBAction func spaSceneChange(sender: AnyObject) {
-        
-        Alamofire.request(.PUT, poolBase + "/resources/sensors/spa/state", parameters: ["value": (sender as UISwitch).on ? "1" : "0"], encoding: .JSONish)
-            .response { (request, response, data, error) in
-                println(request)
-                println(response?.statusCode)
+        manager.PUT(poolBase + "/resources/sensors/spa/state", parameters: ["state": (sender as UISwitch).on ? 1 : 0], success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+            
+            }, failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+                println("FAILED!")
                 println(error)
-        }
+        })
         self.reloadView()
 
     }
@@ -176,16 +182,14 @@ class ViewController: UIViewController {
             return
         }
 
-        var newState = (sender as UISwitch).on ? "1" : "0"
+        var newState = (sender as UISwitch).on ? 1 : 0
         println(newState)
-        
-        Alamofire.request(.PUT, shadeBase + "/resources/sensors/" + objStr + "/state", parameters: ["value": newState], encoding: .JSONish)
-            .response { (request, response, data, error) in
-                println(request)
-                println(response?.statusCode)
-                println(error)
-        }
-        //Alamofire.upload(.PUT, base + "/resources/sensors/" + objStr + "/state", (newState as NSString).dataUsingEncoding(NSUTF8StringEncoding)!)
+        manager.PUT(shadeBase + "/resources/sensors/" + objStr + "/state", parameters: ["state": newState], success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+
+        }, failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+            println("FAILED!")
+            println(error)
+        })
 
     }
 
