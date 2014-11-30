008a39480a0039d143369fa3437099ee2d6b2b73
Eric Buehl <eric.buehl@gmail.com>
2014-11-30 13:20:06 -0800

    basic settings pane

---
+++ shadyglade/DismissSegue.swift
@@ -0,0 +1,17 @@
+//
+//  DismissSegue.swift
+//  shadyglade
+//
+//  Created by Eric Buehl on 11/30/14.
+//  Copyright (c) 2014 Eric Buehl. All rights reserved.
+//
+
+import UiKit
+
+
+@objc(DismissSegue)  // stupid swift/objc name mangling: http://stackoverflow.com/questions/24185345/custom-segue-in-swift
+class DismissSegue : UIStoryboardSegue {
+    override func perform() {
+        self.sourceViewController.presentingViewController??.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
