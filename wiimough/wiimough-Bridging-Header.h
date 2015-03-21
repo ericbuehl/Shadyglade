7b67a0e4e2ba6f67f042c2760bb235e43bb023ff
Eric Buehl <eric.buehl@gmail.com>
2015-03-20 21:37:22 -0700

    basic wiimough today extension

---
+++ wiimough/wiimough-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  wiimough-Bridging-Header.h
+//  shadyglade
+//
+//  Created by Eric Buehl on 3/20/15.
+//  Copyright (c) 2015 Eric Buehl. All rights reserved.
+//
+
+#ifndef shadyglade_wiimough_Bridging_Header_h
+#define shadyglade_wiimough_Bridging_Header_h
+
+#import "AFNetworking.h"
+
+#endif
