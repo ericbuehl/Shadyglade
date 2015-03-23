024e369697788b36a6d1b14351854d04248a4029
Eric Buehl <eric.buehl@gmail.com>
2015-03-23 00:01:49 -0700

    show abbey status with emoji!

--- wiimough/TodayViewController.swift
+++ wiimough/TodayViewController.swift
@@ -12,11 +12,14 @@ import NotificationCenter
 let manager = AFHTTPRequestOperationManager()
 
 class TodayViewController: UIViewController, NCWidgetProviding {
+    @IBOutlet weak var progressSpinner: UIActivityIndicatorView!
+    @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var ToggleButton: UIButton!
         
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view from its nib.
+    self.progressSpinner.hidesWhenStopped = true
     }
     
     override func didReceiveMemoryWarning() {
@@ -24,7 +27,40 @@ class TodayViewController: UIViewController, NCWidgetProviding {
         // Dispose of any resources that can be recreated.
     }
     
+    func startUpdate() {
+        self.statusLabel.text = ""
+        self.progressSpinner.startAnimating()
+    }
+    
+    func updateStatus() {
+        println("updating...")
+        manager.GET("http://10.0.1.8:5000/api/device/Abbey", parameters: [], success: { (operation: AFHTTPRequestOperation!, responseObject: AnyObject!) in
+
+            
+            switch (responseObject as NSDictionary)["state"] as Int {
+            case 0: 
+                self.statusLabel.text = "🌙" //🌚
+            default:
+                self.statusLabel.text = "🌞"
+            }
+            self.progressSpinner.stopAnimating()
+            
+            
+            }, failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+                println("FAILED!")
+                println(error)
+                self.statusLabel.text = "🌵"
+                self.progressSpinner.stopAnimating()
+
+        })
+    
+    }
+    
     func widgetPerformUpdateWithCompletionHandler(completionHandler: ((NCUpdateResult) -> Void)!) {
+        self.startUpdate()
+        self.updateStatus()
+        
+        
         // Perform any setup necessary in order to update the view.
 
         // If an error is encountered, use NCUpdateResult.Failed
@@ -35,11 +71,14 @@ class TodayViewController: UIViewController, NCWidgetProviding {
     }
     @IBAction func didToggle(sender: UIButton) {
         println("DID TOUCH")
+        self.startUpdate()
         manager.POST("http://10.0.1.8:5000/api/device/Abbey", parameters: ["action":"toggle"], success: { (operation: AFHTTPRequestOperation!, responseObject: AnyObject!) in
+            self.updateStatus()
             
             }, failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
                 println("FAILED!")
                 println(error)
+                self.updateStatus()
         })
     }
     
